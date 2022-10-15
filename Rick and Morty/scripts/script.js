e0cef8105a21ec64cbdeb21050e9406cc01a6905
Laura Beleño <vaneessa10@gmail.com>
2022-10-15 14:21:36 +0200

    rickmorty - primera parte

---
+++ Rick and Morty/scripts/script.js
@@ -0,0 +1,15 @@
+
+    fetch("https://rickandmortyapi.com/api/episode/", {
+        
+    })
+    .then(
+        res => res.json ()
+    )
+    .then(
+        res => res.results.map(x => `<p>Episode ${x.id}</p>`)
+    )
+    .then(
+        res => document.getElementById('sidebar').innerHTML = res.join('')
+    )
+
+    
